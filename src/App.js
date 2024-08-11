0ff215634f36fd8cd537de22d92f6e5c9304553d
Alex Hermansyah <118182877+AlexHermansyah@users.noreply.github.com>
2024-08-11 16:18:00 +0000

    ADD

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://example.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SMARTSHOES
+        </a>
+      </header>
+      <p>
+          Application version: {process.env.REACT_APP_VERSION}
+      </p>
+    </div>
+  );
+}
+
+export default App;
