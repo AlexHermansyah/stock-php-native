e1a783521e41148444cc1348fd244d62bc28dc05
AlexHermansyah910 <alexhermansyah910@gmail.com>
2024-08-16 06:32:40 +0000

    tambah tomboh keluar

--- login.php
+++ login.php
@@ -62,7 +62,10 @@ if(!isset($_SESSION['log'])){
                                                 <label for="inputPassword">Password</label>
                                             </div>
                                             <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <button class="btn btn-primary" name = "login">Login ADMIN</button>
+                                                <button class="btn btn-primary" name = "login">Login</button>
+                                            </div>
+                                            <div class="d-flex align-items-right justify-content-between mt-4 mb-0">
+                                                <button class="btn btn-primary" name = "login">keluar</button>
                                             </div>
                                         </form>
                                     </div>
